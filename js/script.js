4eb341e90e8d1a238e0a8ad15bb383e36579d844
MaksymovVolodymyr <maksymov.vd@gmail.com>
2017-08-15 15:42:25 +0300

    scroll

--- js/script.js
+++ js/script.js
@@ -63,5 +63,19 @@
     $('.filter-mob__title').click(function() {
     	$('.filter-mob-wrapper').toggle('slow');
     });  
+
+    //scroll 
+    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+ 
+        var target = this.hash,
+        $target = $(target);
+ 
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    }); 
  	
 });	 	
